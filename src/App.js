7ada1800e5e88e7fd6ea39d96c256b38a0617465
Caglar Acar <caglarracar@gmail.com>
2023-03-24 18:45:34 +0300

    "last commit"

--- src/App.js
+++ src/App.js
@@ -1,54 +1,26 @@
-import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-
-const client = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/users",
-});
-
+const baseURL = "https://jsonplaceholder.typicode.com";
 function App() {
-  const [id, setId] = useState("");
-  const [userList, setUserList] = useState([]);
-  const [result, setResult] = useState("");
+    const getData = async (id) => {
+        try {
+            const usersResponse = await axios.get(`${baseURL}/users?id=${id}`);
+            const user = usersResponse.data[id - 1];
 
-  const handleChange = (e) => {
-    setId(e.target.value);
-  };
+            const postsResponse = await axios.get(`${baseURL}/posts?userId=${id}`);
+            const posts = postsResponse.data[id - 1];
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const user = userList.find((user) => user.id === parseInt(id));
-    if (user) {
-      setResult(JSON.stringify(user, null, 2));
-    } else {
-      setResult(`User with id ${id} not found.`);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await client.get();
-        setUserList(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+            console.log(user , posts);
+        } catch (error) {
+            console.error(error);
+        }
     };
-    fetchData();
-  }, []);
+    // id 1'e sahip kullanıcının posts ve user bilgileri konsola yazdırılacaktır.
+    getData(1);
+    return (
+        <div className="App">
 
-  return (
-      <div className="App">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="id">Lütfen bir Id değeri giriniz:</label>
-          <input type="text" id="id" value={id} onChange={handleChange} />
-          <button type="submit">Gönder</button>
-        </form>
-        <div className="console">
-          <pre>{result}</pre>
         </div>
-      </div>
-  );
+    );
 }
-
 export default App;
